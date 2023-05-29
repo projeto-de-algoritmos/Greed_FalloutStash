53ccdcbe3e1088b7b8ef9cc974d9bfb8ef891015
Mateus Caltabiano <mateus.frauzino@gmail.com>
2023-05-29 17:15:17 -0300

    transformando knapsack em promise
    Co-authored-by: Matheus Soares <matheus.1503@hotmail.com>

--- Scripts/buttons.js
+++ Scripts/buttons.js
@@ -16,22 +16,30 @@ const optimizeBag = () => {
 
   const itensSelecionadosOrdenados = quicksort(itensSelecionados);
 
-  const result = knapsack(
+  knapsackPromise(
     itensSelecionadosOrdenados.length - 1,
     300,
     itensSelecionadosOrdenados,
     0
-  );
-  let itensTexto = "";
+  ).then((result) => {
+    let itensTexto = "";
 
-  result.itens_selecionados.forEach((item) => {
-    itensTexto += `<li>${item.nome} <br> <img src="./Stylesheet/imgs/weight.png" width = "20px"> ${item.peso} <br> <img src="./Stylesheet/imgs/caps-removebg-preview.png" width = "20px"> ${item.valor} </li>`;
+    result.itens_selecionados.forEach((item) => {
+      itensTexto += `<li>${item.nome} <br> <img src="./Stylesheet/imgs/weight.png" width = "20px"> ${item.peso} <br> <img src="./Stylesheet/imgs/caps-removebg-preview.png" width = "20px"> ${item.valor} </li>`;
+    });
+
+    let resultTexto = "";
+
+    if (result.capacidade === 300) {
+      resultTexto = `Você não selecionou nenhum item :( <br> Clique em inventário e selecione os itens que deseja levar!`;
+    } else {
+      resultTexto = `Dos itens selecionados, os mais valiosos que consegue levar custam, ao todo, ${result.valor_total} caps e sobrou ${result.capacidade} de capacidade. <br> Seguem os itens listados abaixo`;
+    }
+
+    knapsackAnswer.innerHTML = resultTexto;
+    knapsackItems.innerHTML = itensTexto;
+    knapsackButton.setAttribute("disabled", "true");
   });
-
-  const resultTexto = `Dos itens selecionados, os mais valiosos que consegue levar custam, ao todo, ${result.valor_total} caps e sobrou ${result.capacidade} de capacidade. <br> Seguem os itens listados abaixo`;
-  knapsackAnswer.innerHTML = resultTexto;
-  knapsackItems.innerHTML = itensTexto;
-  knapsackButton.setAttribute("disabled", "true");
 };
 
 //adiciona classe "active" nos elementos da lista
