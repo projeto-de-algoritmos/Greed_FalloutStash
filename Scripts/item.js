50a7f5b883e50054c3e83984cde95e8ceb1b306e
MtsSrs <matheus.1503@hotmail.com>
2023-05-28 21:28:15 -0300

    Transformando pesos e valores para a base 10
    Co-authored-by: Mateus Caltabiano <mateus.frauzino@gmail.com>

--- Scripts/item.js
+++ Scripts/item.js
@@ -7,50 +7,52 @@ class Item {
 }
 
 const itens = [];
+
 //armas
-const item0 = new Item("Delivery", 10, 774);
+const item0 = new Item("Delivery", 10, 770);
 const item1 = new Item("The Last Minute", 20, 6500);
 const item2 = new Item("Nuka-nuke Launcher", 40, 650);
 const item3 = new Item("Combat Rifle", 20, 1000);
 const item4 = new Item("Combat Shotgun", 30, 1200);
 const item5 = new Item("Fat Man", 40, 5000);
-const item6 = new Item("Furious Power Fist", 15, 1500);
-const item7 = new Item("Gauss Rifle", 25, 2500);
-const item8 = new Item("Gamma Gun", 8, 800);
-const item9 = new Item("Junk Jet", 35, 3000);
-const item10 = new Item("Kremvh's Tooth", 12, 600);
-const item11 = new Item("Molotov Cocktail", 2, 50);
-const item12 = new Item("Overseer's Guardian", 22, 2000);
-const item13 = new Item("Plasma Grenade", 3, 100);
-const item14 = new Item("Rad-X", 1, 20);
-const item15 = new Item("Shishkebab", 18, 700);
-const item16 = new Item("Sledgehammer", 25, 300);
+const item6 = new Item("Furious Power Fist", 20, 1500);
+const item7 = new Item("Gauss Rifle", 30, 2500);
+const item8 = new Item("Gamma Gun", 10, 800);
+const item9 = new Item("Junk Jet", 30, 3000);
+const item10 = new Item("Kremvh's Tooth", 10, 600);
+const item11 = new Item("Molotov Cocktail", 10, 50);
+const item12 = new Item("Overseer's Guardian", 20, 2000);
+const item13 = new Item("Plasma Grenade", 10, 100);
+const item14 = new Item("Rad-X", 10, 20);
+const item15 = new Item("Shishkebab", 20, 700);
+const item16 = new Item("Sledgehammer", 30, 300);
 const item17 = new Item("Tire Iron", 20, 1000);
-const item18 = new Item("Laser Pistol", 12, 600);
-const item19 = new Item("Tesla Rifle", 28, 1800);
+const item18 = new Item("Laser Pistol", 10, 600);
+const item19 = new Item("Tesla Rifle", 30, 1800);
 //armaduras
 const item20 = new Item("Hellfire Power Armor", 60, 10000);
-const item21 = new Item("Leather Armor", 15, 400);
-const item22 = new Item("Minuteman Outfit", 18, 500);
+const item21 = new Item("Leather Armor", 20, 400);
+const item22 = new Item("Minuteman Outfit", 20, 500);
 const item23 = new Item("Raider Power Armor", 50, 3000);
 const item24 = new Item("Assault Gas Mask", 10, 500);
-const item25 = new Item("Carla's Outfit", 18, 200);
+const item25 = new Item("Carla's Outfit", 20, 200);
 const item26 = new Item("Combat Armor", 30, 1200);
-const item27 = new Item("Covenant Security Armor", 25, 600);
-const item28 = new Item("Hazmat Suit", 25, 800);
+const item27 = new Item("Covenant Security Armor", 30, 600);
+const item28 = new Item("Hazmat Suit", 30, 800);
 const item29 = new Item("Military Fatigues", 20, 400);
-const item30 = new Item("T-60 Power Armor", 55, 5000);
+const item30 = new Item("T-60 Power Armor", 60, 5000);
 const item31 = new Item("X-01 Power Armor", 60, 8000);
 //utilitários
-const item32 = new Item("Stimpak", 1, 50);
-const item33 = new Item("Radaway", 1, 60);
-const item34 = new Item("Jet", 1, 40);
-const item35 = new Item("Med-X", 1, 30);
-const item36 = new Item("Yao Guai Meat", 3, 30);
-const item37 = new Item("Bottlecap Mine", 5, 100);
+const item32 = new Item("Stimpak", 10, 50);
+const item33 = new Item("Radaway", 10, 60);
+const item34 = new Item("Jet", 10, 40);
+const item35 = new Item("Med-X", 10, 30);
+const item36 = new Item("Yao Guai Meat", 10, 30);
+const item37 = new Item("Bottlecap Mine", 10, 100);
 const item38 = new Item("Fusion Core", 10, 500);
-const item39 = new Item("XCell", 1, 100);
-const item40 = new Item("Yao Guai Roast", 5, 80);
+const item39 = new Item("XCell", 10, 100);
+const item40 = new Item("Yao Guai Roast", 10, 80);
+
 
 itens.push(
   item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, 
